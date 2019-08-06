d2731f2b1cf3ed890d450abc5628518c37ca25fe
Kaibin <diaokaibin@gmail.com>
2019-08-06 23:54:11 +0800

    通过反射获取带参构造方法并使用

--- src/main/java/com/one/day27/reflect/Demo1_Reflect.java
+++ src/main/java/com/one/day27/reflect/Demo1_Reflect.java
@@ -9,11 +9,11 @@ public class Demo1_Reflect {
         Class clazz1 = Class.forName("com.one.day27.bean.Person");
         Class clazz2 = Person.class;
 
-        Person p = new Person();
-        Class clazz3  = p.getClass();
-
-
-        System.out.println(clazz1==clazz2);
-        System.out.println(clazz3==clazz2);
+//        Person p = new Person();
+//        Class clazz3  = p.getClass();
+//
+//
+//        System.out.println(clazz1==clazz2);
+//        System.out.println(clazz3==clazz2);
     }
 }
--- src/main/java/com/one/day27/reflect/Demo3_Constructor.java
+++ src/main/java/com/one/day27/reflect/Demo3_Constructor.java
@@ -2,18 +2,18 @@ package com.one.day27.reflect;
 
 import com.one.day27.bean.Person;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Demo3_Constructor {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
 
         Class clazz1 = Class.forName("com.one.day27.bean.Person");
-        Class clazz2 = Person.class;
-
-        Person p = new Person();
-        Class clazz3  = p.getClass();
-
-
-        System.out.println(clazz1==clazz2);
-        System.out.println(clazz3==clazz2);
+//        Person p = (Person) clazz1.newInstance();
+//        System.out.printf(" ", p.toString());
+        Constructor constructor = clazz1.getConstructor(String.class, int.class);
+        Person person = (Person) constructor.newInstance("hand", 30);
+        System.out.println(person);
     }
 }
