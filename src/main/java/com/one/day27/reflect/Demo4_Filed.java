8fcc7598aa59a2f30fc10cb5f145a126cbb90e7d
diaokaibin <kaibing.diao@hand-china.com>
2019-08-07 08:34:40 +0800

    update

---
+++ src/main/java/com/one/day27/reflect/Demo4_Filed.java
@@ -0,0 +1,19 @@
+package com.one.day27.reflect;
+
+import com.one.day27.bean.Person;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Demo4_Filed {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+
+        Class clazz1 = Class.forName("com.one.day27.bean.Person");
+//        Person p = (Person) clazz1.newInstance();
+//        System.out.printf(" ", p.toString());
+        Constructor constructor = clazz1.getConstructor(String.class, int.class);
+        Person person = (Person) constructor.newInstance("hand", 30);
+        System.out.println(person);
+    }
+}
