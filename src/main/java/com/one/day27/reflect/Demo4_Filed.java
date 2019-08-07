97db3fa8add08c9b190feee4552f686eb0684c8b
diaokaibin <kaibing.diao@hand-china.com>
2019-08-07 08:40:17 +0800

    update

--- src/main/java/com/one/day27/reflect/Demo4_Filed.java
+++ src/main/java/com/one/day27/reflect/Demo4_Filed.java
@@ -3,17 +3,22 @@ package com.one.day27.reflect;
 import com.one.day27.bean.Person;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Demo4_Filed {
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
 
-        Class clazz1 = Class.forName("com.one.day27.bean.Person");
-//        Person p = (Person) clazz1.newInstance();
-//        System.out.printf(" ", p.toString());
-        Constructor constructor = clazz1.getConstructor(String.class, int.class);
+        Class clazz = Class.forName("com.one.day27.bean.Person");
+        Constructor constructor = clazz.getConstructor(String.class, int.class);
+
         Person person = (Person) constructor.newInstance("hand", 30);
+
+
+        Field f = clazz.getField("name");
+        f.setAccessible(true);
+        f.set(person,"我们");
         System.out.println(person);
     }
 }
