56ad8460a8299da08de2d34dde3de2f29e826476
Kaibin <diaokaibin@gmail.com>
2019-08-22 13:53:28 +0800

    update

---
+++ src/main/java/com/one/time36/lesson3.java
@@ -0,0 +1,14 @@
+package com.one.time36;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lesson3 {
+    public static void main(String[] args) {
+      final List<String> strList = new ArrayList<>();
+        strList.add("hello");
+        strList.add("world");
+
+
+    }
+}
