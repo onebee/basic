783af423a45ff8c4b9716db43587bf7db58d0f98
Kaibin <diaokaibin@gmail.com>
2019-09-05 23:57:32 +0800

    设置画布与图形绘制基础

--- src/main/java/com/one/see/AlgoFrame.java
+++ src/main/java/com/one/see/AlgoFrame.java
@@ -1,6 +1,54 @@
 package com.one.see;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AlgoFrame extends JFrame {
+
+
+    private int canvasWidth;
+    private int canvasHeight;
+
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight) throws HeadlessException {
+        super(title);
+        this.canvasHeight = canvasHeight;
+        this.canvasWidth = canvasWidth;
+
+        AlgoCanvas canvas = new AlgoCanvas();
+        // 设置画布的大小
+        canvas.setPreferredSize(new Dimension(canvasWidth,canvasHeight));
+//        setSize(canvasWidth, canvasHeight);
+        setContentPane(canvas);
+
+        //
+        pack();
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+    public AlgoFrame(String title) throws HeadlessException {
+        this(title, 1024, 768);
+    }
+
+    public int getCanvasWidth() {
+        return canvasWidth;
+    }
+
+    public int getCanvasHeight() {
+        return canvasHeight;
+    }
+
+
+    private class AlgoCanvas extends JPanel{
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+        }
+
+
+
+    }
 }
--- src/main/java/com/one/see/test.java
+++ src/main/java/com/one/see/test.java
@@ -10,11 +10,12 @@ public class test {
         // JAVA 事件分发线程
         EventQueue.invokeLater(() -> {
 
-            JFrame jFrame = new JFrame("Welcome");
-            jFrame.setSize(500, 500);
-            jFrame.setResizable(false);
-            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            jFrame.setVisible(true);
+//            JFrame jFrame = new JFrame("Welcome");
+//            jFrame.setSize(500, 500);
+//            jFrame.setResizable(false);
+//            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            jFrame.setVisible(true);
+            AlgoFrame frame = new AlgoFrame("one", 500, 500);
         });
 
 
