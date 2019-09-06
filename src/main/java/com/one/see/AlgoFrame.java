c13f47999c0b7b4af6e095b4eae988b463f93137
Kaibin <diaokaibin@gmail.com>
2019-09-06 20:52:51 +0800

    Graphics2D的使用

--- src/main/java/com/one/see/AlgoFrame.java
+++ src/main/java/com/one/see/AlgoFrame.java
@@ -2,6 +2,7 @@ package com.one.see;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 public class AlgoFrame extends JFrame {
 
@@ -16,7 +17,7 @@ public class AlgoFrame extends JFrame {
 
         AlgoCanvas canvas = new AlgoCanvas();
         // 设置画布的大小
-        canvas.setPreferredSize(new Dimension(canvasWidth,canvasHeight));
+//        canvas.setPreferredSize(new Dimension(canvasWidth,canvasHeight));
 //        setSize(canvasWidth, canvasHeight);
         setContentPane(canvas);
 
@@ -46,9 +47,31 @@ public class AlgoFrame extends JFrame {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D)g;
+
+            Ellipse2D circle = new Ellipse2D.Double(50,50,300,300);
+
+            int strokeWidth = 10;
+
+            g2d.setStroke(new BasicStroke(strokeWidth));
+
+            g2d.setColor(Color.red);
+            g2d.draw(circle);
+
+
+            g2d.setColor(Color.yellow);
+            Ellipse2D circle2 = new Ellipse2D.Double(60,60,280,280);
+            g2d.fill(circle2);
+            // 个性化绘制代码
+//            g2d.drawOval(50,50,300,300);
         }
 
 
+        @Override
+        public Dimension getPreferredSize() {
 
+            return new Dimension(canvasWidth,canvasHeight);
+        }
     }
 }
