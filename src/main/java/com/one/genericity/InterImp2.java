c27ded255d708988d30ef247695208f88e7eec8e
Kaibin <diaokaibin@gmail.com>
2019-10-02 15:32:15 +0800

    add 泛型sample

--- src/main/java/com/one/genericity/InterImp2.java
+++ src/main/java/com/one/genericity/InterImp2.java
@@ -1,10 +1,13 @@
 package com.one.genericity;
 
+import java.util.Collections;
+
 public class InterImp2<E> implements Inter<E> {
 
     @Override
     public void show(E e) {
 
         System.out.println("hhh" + e);
+//        Collections.shuffle();
     }
 }
