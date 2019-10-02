06be5cf97a73bedf2431522bd75008fb5f8075d5
Kaibin <diaokaibin@gmail.com>
2019-10-02 14:02:02 +0800

    update

--- src/main/java/com/one/calendar/main.java
+++ src/main/java/com/one/calendar/main.java
@@ -22,5 +22,16 @@ public class main {
         // 将日程的天数移动到需要绘制的第一天
         calendar.add(Calendar.DAY_OF_MONTH,-prevDays);
 
+        int maxCellCount = 6*7;
+        while (cells.size() < maxCellCount) {
+
+            cells.add(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_MONTH,1);
+        }
+
+        for (int i = 0; i < cells.size(); i++) {
+            System.out.println(cells.get(i).toString());
+        }
+
     }
 }
