5fb309c8643f2db7dfeadb750f0d67d793cd4b66
diaokaibin <kaibing.diao@hand-china.com>
2019-10-25 23:29:59 +0800

    update

---
+++ src/main/java/com/one/leetcode/main111.java
@@ -0,0 +1,12 @@
+package com.one.leetcode;
+
+public class main111 {
+
+
+    public static void main(String[] args) {
+        int day = 31, month = 8, year = 2019;
+        Solution solution = new Solution();
+        String dayOfTheWeek = solution.dayOfTheWeek(day, month, year);
+        System.out.println(dayOfTheWeek);
+    }
+}
