d492e909dbb84de645029d9ad4a4e1042b5c7263
Kaibin <diaokaibin@gmail.com>
2019-11-01 16:38:31 +0800

    update

--- src/main/java/com/one/leetcode/Solution.java
+++ src/main/java/com/one/leetcode/Solution.java
@@ -2,16 +2,17 @@ package com.one.leetcode;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class Solution {
 
     public String dayOfTheWeek(int day, int month, int year) {
         Calendar c = Calendar.getInstance();
         c.set(Calendar.YEAR,year);
-        c.set(Calendar.MONTH,month+1);
-        c.set(Calendar.DAY_OF_MONTH,day);
+        c.set(Calendar.MONTH,month-1);
+        c.set(Calendar.DATE,day);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("EEE");
+        SimpleDateFormat sdf = new SimpleDateFormat(" EEEE",Locale.ENGLISH);
         String format = sdf.format(c.getTime());
         return format;
     }
