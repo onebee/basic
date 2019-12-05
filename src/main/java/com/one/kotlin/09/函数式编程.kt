14e8d65a8ab18b92305b69f37fb192967e154c44
Kaibin <diaokaibin@gmail.com>
2019-12-05 23:57:08 +0800

    update

---
+++ src/main/java/com/one/kotlin/09/函数式编程.kt
@@ -0,0 +1,17 @@
+package com.one.kotlin.`09`
+
+/**
+ * (T) -> Unit 接收的参数的类型
+ * (String) -> Unit 函数类型,参数为字符串,返回值是Unit
+ */
+fun main(args: Array<String>) {
+
+
+    var name = listOf<String>("tom", "jerry", "lucy")
+    name.forEach(print)
+
+}
+
+var print = fun (name: String): Unit {
+    println(name)
+}
