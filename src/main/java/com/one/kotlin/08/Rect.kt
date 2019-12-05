ca052d44ad311fb8319a021ece6f969e294fa91a
Kaibin <diaokaibin@gmail.com>
2019-12-05 23:05:58 +0800

    update

---
+++ src/main/java/com/one/kotlin/08/Rect.kt
@@ -0,0 +1,11 @@
+package com.one.kotlin.`08`
+
+class Rect(var height: Int, var witdh: Int) {
+
+
+    fun smile() {
+        println(" 小的")
+    }
+
+
+}
--- src/main/java/com/one/kotlin/08/demo.kt
+++ src/main/java/com/one/kotlin/08/demo.kt
@@ -1,2 +1,13 @@
 package com.one.kotlin.`08`
 
+fun main(args: Array<String>) {
+
+
+
+
+    var rec = Rect(20,35)
+    println("高 : ${rec.height}")
+    println(rec.witdh)
+
+    rec.smile()
+}
