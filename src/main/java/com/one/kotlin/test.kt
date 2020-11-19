66f543075dfd4eaaa287ce9475f4628fbb4739c9
Kaibin <diaokaibin@gmail.com>
2020-11-19 09:22:12 +0800

    update

---
+++ src/main/java/com/one/kotlin/test.kt
@@ -0,0 +1,12 @@
+package com.one.kotlin
+
+import kotlin.math.hypot
+
+fun main(args: Array<String>) {
+
+    var x = hypot(6f,8f)
+
+    println(x)
+
+
+}
--- src/main/java/com/one/thread/Main.java
+++ src/main/java/com/one/thread/Main.java
@@ -1,5 +1,6 @@
 package com.one.thread;
 
+import java.util.Random;
 import java.util.concurrent.*;
 
 public class Main {
@@ -10,9 +11,29 @@ public class Main {
 
 //        runnable();
 
-        callable();
-    }
+//        callable();
 
+//        Random random = new Random();
+//        boolean b = random.nextBoolean();
+//
+//        System.out.println(b)
+
+        String aa="sdad中国,.ss。";
+        char[] charArray = aa.toCharArray();
+        for (char c : charArray) {
+
+            String  cnorEn = isCnorEn(c);
+            System.out.println(c+":"+cnorEn);
+        }
+    }
+    static String isCnorEn(char c){
+        if(c >= 0x0391 && c <= 0xFFE5) //中文字符
+            return "中文";
+        if(c>=0x0000 && c<=0x00FF){ //英文字符
+            return "英文";
+        }
+        return "其他";
+    }
 
     static void thread() {
         Thread thread = new Thread() {
